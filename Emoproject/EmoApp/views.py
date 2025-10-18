a9a20156b9b60bff54c08228989944af2d88f147
yoshitakesana <fko2447057@stu.o-hara.ac.jp>
2025-10-18 17:12:32 +0900

    add

--- Emoproject/EmoApp/views.py
+++ Emoproject/EmoApp/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
 class IndexView(TemplateView):
-    template_name='index.html'
+    template_name='EmoApp/index.html'
